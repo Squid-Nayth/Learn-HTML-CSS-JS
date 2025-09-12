e6459d6aa807fba14847330fb415d3bc6c4079a4
Nathan Michel <nathannkombe@gmail.com>
2025-09-12 05:08:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@
     console.log(StatutMarital);
     
     
-} */
+} 
 const STATUT_MARITAL = {
     CELIBATAIRE: "celibate",
     MARIE: "married"
@@ -42,4 +42,57 @@ let personne = {
 console.log(personne.statusMarital);
 personne.toggleStatusMarital();
 console.log(personne.statusMarital);
+
+console.log("----------------------------------------------------------") */
+
+
+
+let utilisateur = {
+    nom: "Nathan",
+    prenom: "Michel",
+    age: 20,
+    ville: "Paris",
+    estMarie: false,
+    estEtudiant: false,
+
+
+
+
+
+
+    
+}
+let anneeNaissance = 2025 - utilisateur.age
+let joursVécus = utilisateur * 365
+age++ 
+if(utilisateur.age > 18 ){
+    console.log("tu es majeur")
+}else {
+    console.log("tu es mineur");
+}
+if(utilisateur.age === 30 ){
+    console.log("tu as 30 ans")
+}else {
+    console.log("tu as moins de 30 ans");
+}
+if(utilisateur.estEtudiant == false ){
+    console.log("tu n'es pas étudiant")
+}else {
+    console.log("tu es étudiant");
+}
+
+
+
+
+console.log(typeof utilisateur.nom);
+console.log(typeof utilisateur.prenom);
+console.log(typeof utilisateur.age);
+console.log(typeof utilisateur.ville);
+console.log(typeof utilisateur.estMarie);
+console.log(typeof utilisateur.estEtudiant);
+console.log(anneeNaissance);
+
+
+
+
 
